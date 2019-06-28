e17ad66f33b20dec0cba3b24a13028f8162c7f41
Daryl Liu <lj3lj3@gmail.com>
2019-06-28 16:59:42 +0800

    Add user info and video info getter

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"io/ioutil"
-	"net/http"
 	"net/url"
 	"regexp"
+	"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
 )
 
-type douyinMessage struct {
+type tiktokMessage struct {
 	Id  int    `json:"id"`
 	Url string `json:"url"`
 }
@@ -24,6 +24,7 @@ type douyinMessage struct {
 var db *sqlx.DB
 var client *redis.Client
 var concurrentLimit chan byte
+var bgImageReg *regexp.Regexp
 
 func init() {
 	// init redis
@@ -47,12 +48,14 @@ func init() {
 
 	// init concurrent limit channel
 	concurrentLimit = make(chan byte, 10)
+
+	bgImageReg, _ = regexp.Compile(`background-image:url\((.+)\)`)
 }
 
 func main() {
 	fmt.Println("waiting for message in the queue...")
 	for {
-		brPop := client.BRPop(time.Hour*24*365, "likeGetter")
+		brPop := client.BRPop(time.Hour*24*365, "tiktokGetter")
 		result, err := brPop.Result()
 		if err != nil {
 			fmt.Printf("error in getting results from brPop, %v \n", err)
@@ -62,7 +65,7 @@ func main() {
 		}
 
 		fmt.Printf("read data by using brPop: %v \n", result)
-		msg := &douyinMessage{}
+		msg := &tiktokMessage{}
 		// the 0 index in the result is key name, the 1 index in the result is value
 		if err = json.Unmarshal([]byte(result[1]), msg); err != nil {
 			fmt.Printf("error in unmarshal results from brPop, %v \n", err)
@@ -76,20 +79,25 @@ func main() {
 			// decrease count in buffer
 			defer func() {
 				<-concurrentLimit
+
+				// Recover from panic
+				if err := recover(); err != nil {
+					fmt.Printf("RECOVERED: %v, %s\n", err, debug.Stack())
+				}
 			}()
-			getDouyinData(msg)
+			getTiktokData(msg)
 		}()
 	}
 }
 
-func getDouyinData(message *douyinMessage) {
+func getTiktokData(message *tiktokMessage) {
 	// validate message
 	urlTmp, err := url.Parse(message.Url)
 	if err != nil {
 		fmt.Printf("[%d]error when parsing urlTmp: %s \n", message.Id, err)
 		return
 	} else if urlTmp.Host != "v.douyin.com" && urlTmp.Host != "www.iesdouyin.com" {
-		fmt.Printf("[%d]url is not from douyin: %s \n", message.Id, urlTmp.Host)
+		fmt.Printf("[%d]url is not from tictok: %s \n", message.Id, urlTmp.Host)
 		return
 	}
 
@@ -126,11 +134,102 @@ func getDouyinData(message *douyinMessage) {
 	}
 	data["comment"] = strconv.Itoa(commentCount)
 
+	// Get user info
+	userInfo, err := getUserInfo(document, message)
+	if err != nil {
+		return
+	}
+
+	// Get video info
+	videoInfo, err := getVideoInfo(document, message)
+	if err != nil {
+		return
+	}
+
 	// write back to database
-	writeDouyinData(message, data)
+	writeTiktokData(message, mergeMaps(data, userInfo, videoInfo))
 }
 
-func getCount(document *goquery.Document, message *douyinMessage, selector string) (int, error) {
+func getVideoInfo(document *goquery.Document, message *tiktokMessage) (map[string]string, error) {
+	data := make(map[string]string)
+
+	// Get video title
+	data["video_title"] = document.Find("#videoUser>.user-title").Text()
+
+	// Get video poster
+	style, exists := document.Find("#videoPoster").Attr("style")
+	if exists {
+		// Load poster
+		matched := bgImageReg.FindStringSubmatch(style)
+		if len(matched) == 0 {
+			fmt.Printf("[%d]errror in finding video poster, style: %s \n", message.Id, style)
+			data["video_poster"] = ""
+		} else {
+			// Download video poster
+			filePath, err := downloadFile(matched[1], strconv.Itoa(message.Id)+"_video_poster")
+			if err != nil {
+				return nil, err
+			}
+			data["video_poster"] = filePath
+		}
+	}
+
+	// Get video
+	src, exists := document.Find("#theVideo").Attr("src")
+	if exists {
+		// Download video
+		filePath, err := downloadFile(src, strconv.Itoa(message.Id)+"_video")
+		if err != nil {
+			return nil, err
+		}
+		data["video"] = filePath
+	}
+
+	return data, nil
+}
+
+func getUserInfo(document *goquery.Document, message *tiktokMessage) (map[string]string, error) {
+	data := make(map[string]string)
+	videoUserNode := document.Find("#videoUser")
+
+	// Get user avatar
+	style, exists := videoUserNode.Find(".user-avator").Attr("style")
+	if exists {
+		// Load avatar
+		matched := bgImageReg.FindStringSubmatch(style)
+		if len(matched) == 0 {
+			fmt.Printf("[%d]errror in finding user avatar, style: %s \n", message.Id, style)
+			data["user_avatar"] = ""
+		} else {
+			data["user_avatar"] = matched[1]
+		}
+	}
+	// Get user name
+	data["user_name"] = videoUserNode.Find(".user-info>.user-info-name").Text()
+	// Get tiktok id
+	// EDIT: tiktok id can be changed to letter
+	tiktokIdHtml, err := videoUserNode.Find(".user-info>.user-info-id").Html()
+	if err != nil {
+		fmt.Printf("[%d]errror in getting html of user tiktok id: %v \n", message.Id, err)
+	}
+	// Find and replace
+	reg, _ := regexp.Compile(`<i class="icon iconfont\s?">\s?;(.{6})\s?</i>`)
+	tiktokIdStr := reg.ReplaceAllStringFunc(tiktokIdHtml, func(s string) string {
+		matches := reg.FindStringSubmatch(s)
+		if len(matches) > 1 {
+			return strconv.Itoa(getDigitFromFontString("&#" + matches[1])) // Replace i DOM node with right number
+		}
+		return s
+	})
+	// Remove useless character and trim space
+	data["user_tiktok_id"] = strings.TrimSpace(strings.Replace(tiktokIdStr, "抖音ID:", "", 1))
+
+	fmt.Printf("[%d]user info: %s \n", message.Id, data)
+
+	return data, nil
+}
+
+func getCount(document *goquery.Document, message *tiktokMessage, selector string) (int, error) {
 	countStr := ""
 	document.Find(selector).Each(func(i int, selection *goquery.Selection) {
 		countStr += strconv.Itoa(getDigitFromFontString("&#" + selection.Text()[2:]))
@@ -147,16 +246,7 @@ func getCount(document *goquery.Document, message *douyinMessage, selector strin
 }
 
 func getUrlContent(url string) ([]byte, error) {
-	// ready to get url content
-	var client = &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	// set up header, let server treat us as mobile browser
-	req.Header.Set("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1")
-
-	response, err := client.Do(req)
+	response, err := httpGet(url)
 	if err != nil {
 		return nil, err
 	}
@@ -172,9 +262,17 @@ func getUrlContent(url string) ([]byte, error) {
 	return body, nil
 }
 
-func writeDouyinData(message *douyinMessage, data map[string]string) {
-	result, err := db.NamedExec("UPDATE df_signup SET likes = :likeCount, comments = :commentCount WHERE id = :id", map[string]interface{}{
+func writeTiktokData(message *tiktokMessage, data map[string]string) {
+	result, err := db.NamedExec("UPDATE df_signup SET likes = :likeCount, comments = :commentCount,"+
+		"user_name = :userName, user_tiktok_id = :userTiktokId, user_avatar = :userAvatar, video_poster = :videoPoster, "+
+		"video = :video, video_title = :videoTitle WHERE id = :id", map[string]interface{}{
 		"likeCount":    data["like"],
+		"userName":     data["user_name"],
+		"userTiktokId": data["user_tiktok_id"],
+		"userAvatar":   data["user_avatar"],
+		"videoPoster":  data["video_poster"],
+		"video":        data["video"],
+		"videoTitle":   data["video_title"],
 		"commentCount": data["comment"],
 		"id":           message.Id,
 	})
--- utils.go
+++ utils.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
 	"strings"
+	"time"
 )
+
+const userAgent string = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+
+var rootPath string
+
+func init() {
+	executable, _ := os.Executable()
+	rootPath = path.Dir(executable)
+}
 
 func getDigitFromFontString(fontStr string) int {
 	fontStr = strings.ToUpper(fontStr)[3:7]
@@ -32,4 +46,84 @@ func getDigitFromFontString(fontStr string) int {
 		fmt.Printf("not a valid font string: %s", fontStr)
 		return 0
 	}
+}
+
+func mergeMaps(maps ...map[string]string) map[string]string {
+	finalMap := make(map[string]string)
+	for _, mapTmp := range maps {
+		for key, value := range mapTmp {
+			finalMap[key] = value // Overwrite
+		}
+	}
+	return finalMap
+}
+
+func httpGet(url string) (*http.Response, error) {
+	// ready to get url content
+	var client = &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Printf("error in creating requests: %v \n", err)
+		return nil, err
+	}
+	// set up header, let server treat us as mobile browser
+	req.Header.Set("User-Agent", userAgent)
+
+	response, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("error in getting response: %v \n", err)
+		return nil, err
+	}
+	return response, nil
+}
+
+func downloadFile(url string, fileName string) (string, error) {
+	// Create folders
+	folder := rootPath + "/wwwroot/upload/getter/"
+	if err := os.MkdirAll(folder, 0755); err != nil {
+		fmt.Printf("error in making folders : %v \n", err)
+		return "", err
+	}
+
+	// ready to get url content
+	resp, err := httpGet(url)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	// Get extension from response
+	extension := "jpg"
+	contentTypes := strings.Split(resp.Header.Get("Content-Type"), "/")
+	if len(contentTypes) > 1 {
+		extension = contentTypes[1]
+	}
+
+	// Create file
+	file := folder + fileName + "." + extension
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		// Not exists
+		startTime := time.Now()
+		fmt.Printf("downloading file: %s \n", fileName)
+
+		filePtr, err := os.Create(file)
+		if err != nil {
+			fmt.Printf("error in creating file: %v \n", err)
+			return "", err
+		}
+		defer func() {
+			_ = filePtr.Close()
+		}()
+
+		if _, err := io.Copy(filePtr, resp.Body); err != nil {
+			fmt.Printf("error in writring file: %v \n", err)
+			return "", err
+		}
+
+		fmt.Printf("downloading file DONE: %s,%s, time cost:%d \n", fileName, url, time.Since(startTime))
+	}
+
+	return "/upload/getter/" + fileName + "." + extension, nil
 }
